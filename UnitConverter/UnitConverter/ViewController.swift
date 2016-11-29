d21058c372f32808e020274ad975734606535e37
alexbaek <alexbaek31@gmail.com>
2016-11-29 16:01:17 +0900

    개발 완료.
    뷰에 입력을 받고 결과값까지 화면에 나타내도록 처리.

--- UnitConverter/UnitConverter/ViewController.swift
+++ UnitConverter/UnitConverter/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tempText: UITextField!
+    @IBOutlet weak var resultText: UITextField!
+    
+    @IBAction func convertTemp(_ sender: AnyObject) {
+        
+        if let fahreheit = Double(tempText.text!) {
+            let celsius = (fahreheit - 32) / 1.8
+            let result = "Celsius \(celsius)"
+            resultText.text = result
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
